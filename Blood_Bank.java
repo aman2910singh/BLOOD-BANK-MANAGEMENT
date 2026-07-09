296110c6b1fce550687e0b82df82d047640c6d3f
aman2910singh <amansingh7024442385@gmail.com>
2026-07-09 18:51:00 +0530

    Add all files

---
+++ Blood_Bank.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+class Donor {
+    int donorId;
+    String name;
+    String bloodGroup;
+    int units;
+     
+    static int idCounter;
+
+    public Donor(String name ,String bloodGroup,int units){
+        this.name=name;
+        this.bloodGroup=bloodGroup;
+        this.units=units;
+    }
+}
+
+public class Blood_Bank {
+
+    
+    public static void main(String[] args) {
+        int choice=5;
+
+        Scanner sc=new Scanner(System.in);
+        
+        do{
+            System.out.println("\n====BLOOD BANK MANAGEMENT SYSTEM====");
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            switch (choice) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+                default:
+                    break;
+            }
+
+        }while(choice!=5);
+        
+        
+    }
+}
